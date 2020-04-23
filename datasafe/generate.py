376f3c05d76be8ae811b8e179ad7d19cdcfbc59c
Till Biskup <till@till-biskup.de>
2020-04-23 14:25:38 +0200

    Refactor generate to class

--- datasafe/generate.py
+++ datasafe/generate.py
@@ -1,78 +1,81 @@
 import os
 
 
-def find_last_id(path=''):
+class Generate:
     """
-    Find last (numeric) ID used in a particular directory of the datasafe
+    Class for whatever...
 
-    Return last element of a sorted list of directory contents, assuming the
-    directory to only contain subdirectories with numeric IDs.
-
-    In case there is no numeric ID yet in the directory, it returns 0.
-
-    Parameters
+    Attributes
     ----------
     path : :class:`str`
-        directory path to look for latest ID in
-
-    Returns
-    -------
-    last_id : :class:`int`
-        Highest ID in current directory
+        base directory for the datasafe
 
     """
-    directory_contents = os.listdir(path)
-    # Important: Convert first to integers, then sort
-    directory_contents = list(map(int, directory_contents))
-    if not directory_contents:
-        highest_id = 0
-    else:
-        highest_id = sorted(directory_contents)[-1]
-    return highest_id
+
+    def __init__(self):
+        self.path = ''
+
+    def find_last_id(self):
+        """
+        Find last (numeric) ID used in a particular directory of the datasafe
+
+        Return last element of a sorted list of directory contents, assuming the
+        directory to only contain subdirectories with numeric IDs.
+
+        In case there is no numeric ID yet in the directory, it returns 0.
+
+        Returns
+        -------
+        last_id : :class:`int`
+            Highest ID in current directory
+
+        """
+        directory_contents = os.listdir(self.path)
+        # Important: Convert first to integers, then sort
+        directory_contents = list(map(int, directory_contents))
+        if not directory_contents:
+            highest_id = 0
+        else:
+            highest_id = sorted(directory_contents)[-1]
+        return highest_id
+
+    def create_new_directory(self, new_dir=''):
+        """
+        Create a new directory within the given path.
+
+        Parameters
+        ----------
+        new_dir : :class:`str`
+            Name of new directory to create
+
+        """
+        complete_path = os.path.join(self.path, new_dir)
+        os.mkdir(path=complete_path)
+        return
+
+    def create_directory_for_new_dataset(self):
+        """
+        Create directory in datasafe for new dataset.
+
+        Returns
+        -------
+        new_id : :class:`int`
+            ID of newly created directory
+
+        """
+        highest_id = self.find_last_id()
+        new_id = highest_id + 1
+        self.create_new_directory(new_dir=str(new_id))
+        return new_id
 
 
-def create_new_directory(path='', new_dir=''):
-    """
-    Create a new directory within the given path.
+if __name__ == '__main__':
+    temporary_datasafe = '../tests/files/tmpsafe'
 
-    Parameters
-    ----------
-    path : :class:`str`
-        Directory to create the new directory in
+    irgendwas = Generate()
+    irgendwas.path = temporary_datasafe
 
-    new_dir : :class:`str`
-        Name of new directory to create
+    last_id = irgendwas.find_last_id()
+    print(last_id)
 
-    """
-    complete_path = os.path.join(path, new_dir)
-    os.mkdir(path=complete_path)
-    return
-
-
-def create_directory_for_new_dataset(path=''):
-    """
-    Create directory in datasafe for new dataset.
-
-    Parameters
-    ----------
-    path : :class:`str`
-        Path to create new directory in
-
-    Returns
-    -------
-    new_id : :class:`int`
-        ID of newly created directory
-
-    """
-    highest_id = find_last_id(path=temporary_datasafe)
-    new_id = highest_id + 1
-    create_new_directory(path=temporary_datasafe, new_dir=str(new_id))
-    return new_id
-
-
-#temporary_datasafe = 'tmpsafe'
-
-#last_id = find_last_id(path=temporary_datasafe)
-#print(last_id)
-
-#print(create_directory_for_new_dataset(path=temporary_datasafe))
+    print(irgendwas.create_directory_for_new_dataset())
