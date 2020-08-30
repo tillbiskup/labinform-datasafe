c3879d2a52d88ea1f9ea4cff1b69928b0173eb74
Till Biskup <till@till-biskup.de>
2020-08-30 15:59:36 +0200

    Add logo to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -93,6 +93,10 @@ html_theme_options = {
     'style_external_links': True,
 }
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = './logo.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
