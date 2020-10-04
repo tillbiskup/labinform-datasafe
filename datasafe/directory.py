96f77518c03c506b6f4b8d102704334d4cff0fa1
Till Biskup <till@till-biskup.de>
2020-10-04 19:45:20 +0200

    Implement storage backend test-first

--- datasafe/directory.py
+++ datasafe/directory.py
@@ -1,7 +1,116 @@
 import os
+import shutil
+import tempfile
 
 
-class Generator:
+class Error(Exception):
+    """Base class for exceptions in this module."""
+
+    pass
+
+
+class MissingPathError(Error):
+    """Exception raised when no path is provided
+
+    Attributes
+    ----------
+    message : :class:`str`
+        explanation of the error
+
+    """
+
+    def __init__(self, message=''):
+        super().__init__()
+        self.message = message
+
+
+class MissingContentError(Error):
+    """Exception raised when no content is provided
+
+    Attributes
+    ----------
+    message : :class:`str`
+        explanation of the error
+
+    """
+
+    def __init__(self, message=''):
+        super().__init__()
+        self.message = message
+
+
+class StorageBackend:
+    def __init__(self):
+        self.root_directory = ''
+        self.path = ''
+
+    def working_path(self, path=''):
+        """
+        Full path to working directory in datasafe
+
+        Returns
+        -------
+        working_path : :class:`str`
+            full path to work on
+        """
+        return os.path.join(self.root_directory, path)
+
+    def create(self, path=''):
+        if not path:
+            raise MissingPathError
+        os.makedirs(self.working_path(path))
+
+    def exists(self, path=''):
+        return os.path.exists(self.working_path(path))
+
+    def isempty(self, path=''):
+        if not os.path.exists(self.working_path(path)):
+            raise FileNotFoundError
+        return not os.listdir(self.working_path(path))
+
+    def remove(self, path='', force=False):
+        if force:
+            shutil.rmtree(self.working_path(path))
+        else:
+            os.rmdir(self.working_path(path))
+
+    def get_highest_id(self, path=''):
+        directory_contents = os.listdir(self.working_path(path))
+        # Important: Convert first to integers, then sort
+        directory_contents = list(map(int, directory_contents))
+        if not directory_contents:
+            highest_id = 0
+        else:
+            highest_id = sorted(directory_contents)[-1]
+        return highest_id
+
+    def create_next_id(self, path=''):
+        self.create(os.path.join(path, str(self.get_highest_id(path) + 1)))
+
+    def deposit(self, path='', content=None):
+        if not path:
+            raise MissingPathError(message='No path provided.')
+        if not content:
+            raise MissingContentError(message='No content provided to deposit.')
+        tmpfile = tempfile.mkstemp(suffix='.zip')
+        with open(tmpfile[1], "wb") as f:
+            f.write(content)
+        shutil.unpack_archive(tmpfile[1], self.working_path(path))
+        os.remove(tmpfile[1])
+
+    def retrieve(self, path=''):
+        if not path:
+            raise MissingPathError(message='No path provided.')
+        tmpfile = tempfile.mkstemp()
+        zip_archive = shutil.make_archive(base_name=tmpfile[1], format='zip',
+                                          root_dir=self.working_path(path))
+        with open(zip_archive, 'rb') as zip_file:
+            contents = zip_file.read()
+        os.remove(tmpfile[1] + '.zip')
+        return contents
+
+
+class OldGenerator:
     """
     Class for (physically) creating directories in the datasafe
 
@@ -106,7 +215,7 @@ if __name__ == '__main__':
     datasafe_root = '../tests/files/'
     working_dir = 'tmpsafe'
 
-    irgendwas = Generator()
+    irgendwas = StorageBackend()
     irgendwas.path = working_dir
     irgendwas.base_directory = datasafe_root
 
